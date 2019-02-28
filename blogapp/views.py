d07f4d935119b580ab1fd8879ada61c965e646b8
alstn2468 <alstn2468_@naver.com>
2019-02-28 12:55:57 +0900

    Add paging function in search page

--- blogapp/views.py
+++ blogapp/views.py
@@ -5,13 +5,19 @@ from .models import Blog
 from .forms import BlogPost
 
 
-def home(request):
-    blog_list = Blog.objects.all()
-
-    paginator = Paginator(blog_list, 3)
+def paging(request, list, num):
+    paginator = Paginator(list, num)
     page = request.GET.get('page')
 
     posts = paginator.get_page(page)
+
+    return posts
+
+
+def home(request):
+    blog_list = Blog.objects.all()
+
+    posts = paging(request, blog_list, 3)
 
     return render(request, "home.html", {
         "posts": posts,
@@ -45,8 +51,10 @@ def search(request):
     if keyword:
         post_list = post_list.filter(title__icontains=keyword)
 
+    posts = paging(request, post_list, 3)
+
     return render(request, "search.html", {
-        'post_list': post_list,
+        'posts': posts,
         'keyword': keyword
     })
 
